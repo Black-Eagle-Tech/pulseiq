8b2e2bfbb4033d5146f3d11e9ed2c801927c3e1e
Mauro Vicens <blackeagledhs@gmail.com>
2026-05-14 10:36:17 -0300

    change vite index.css to fix styles

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
 
+import tailwindcss from '@tailwindcss/vite'
+
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
 
   base: './',
 
